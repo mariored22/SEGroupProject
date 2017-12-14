940ed61e007f8015c6ccf78a0931338ba268cbb2
akhilnukala <32026429+akhilnukala@users.noreply.github.com>
2017-12-13 21:34:50 -0600

    Add files via upload

--- Spine.cpp
+++ Spine.cpp
@@ -1,35 +1,50 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
 #include "Spine.h"
 
+int Spine::created= 0;
+int Spine::destroyed = 0;
 
-/**
- * runtime representation of an immutable list of goals
- * together with top of heap and trail pointers
- * and current clause tried out by head goal
- * as well as registers associated to it
- *
- * note that parts of this immutable lists
- * are shared among alternative branches
- */
+Spine::Spine(const int* gs0, const int gslength, const int base, std::vector<int>& gs, const int ttop, const int k, std::vector<int>* cs)
+{
+	hd = gs0[0];
+	this->base = base;
+	this->gs = IntList_tail(IntList_app(gs0, gslength, gs)); // prepends the goals of clause with head hs
+	this->ttop = ttop;
+	this->k = k;
+	this->_cs = cs;
+	this->xsNull = true;
 
+	created++;
+}
 
-Spine::Spine( vector<int> gs0,  int base,  IntList *gs,  int ttop,  int k, vector <int> cs) {
-    hd = gs0[0];
-    this->base = base;
-    this->gs = IntList::tail(IntList::app(gs0, gs)); // prepends the goals of clause with head hs
-    this->ttop = ttop;
-    this->k = k;
-	this->cs = new vector<int>();
-	for (int x : cs)
-		this->cs->push_back(x);
-  }
+Spine::Spine(const int hd, const int ttop)
+{
+	this->hd = hd;
+	base = 0;
+	//gs = NULL;
+	this->ttop = ttop;
 
+	k = -1;
+	_cs = NULL;
+	xsNull = true;
 
-Spine::Spine( int hd,  int ttop) {
-    this->hd = hd;
-    base = 0;
-    gs = NULL;
-    this->ttop = ttop;
+	created++;
+}
 
-    k = -1;
-    cs = NULL;
-  }
+Spine::~Spine()
+{	
+	//IntList::deleteList(gs);	
+	destroyed++;
+}
+
+int Spine::cs(int index) const
+{
+	return _cs->at(index);
+}
+
+int Spine::csLength() const
+{
+	return (int)_cs->size();
+}
