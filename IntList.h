478937ac0de2b88900370905205daffb819ca29a
akhilnukala <32026429+akhilnukala@users.noreply.github.com>
2017-12-13 21:38:07 -0600

    Add files via upload

--- IntList.h
+++ IntList.h
@@ -1,32 +1,11 @@
-#ifndef __INTLIST_H__
-#define __INTLIST_H__
+#ifndef _INTLIST_
+#define _INTLIST_
 
-#include <string>
 #include <vector>
 #include "IntStack.h"
 
-using namespace std;
-
-class IntList {
-
-private:
-
-	int _head;
-	IntList * _tail;
-	
-	IntList(int head);
-	IntList(int X, IntList * Xs);
-
-public:
-
-	static bool isEmpty(IntList *Xs);
-	static int head(IntList *Xs);
-	static IntList * tail(IntList * Xs);
-	static IntList * cons(int X, IntList *Xs);
-	static IntList * app(vector<int> xs, IntList * Ys);
-	static IntStack * toInts(IntList * Xs);
-	static int len(IntList *Xs);
-	string toString();
-};
+int IntList_head(const std::vector<int> input);
+std::vector<int> IntList_tail(const std::vector<int> input);
+std::vector<int> IntList_app(const int* xs, int xslength, const std::vector<int> input);
 
 #endif
