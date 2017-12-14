45e91cd30cf229b5b3d489c10cef411c64dc6e83
aptr288 <aptr288@gmail.com>
2017-12-13 21:29:06 -0600

    Add files via upload

--- IntStack.cpp
+++ IntStack.cpp
@@ -1,137 +1,137 @@
 #include "IntStack.h"
+#include <cstdlib>
+#include <cstring>
 
-#pragma warning(disable: 4996)
+IntStack::IntStack(const int size) 
+{
+	stack = new int[size];
+	stackLength = size;
+	clear();
+}
 
-	IntStack::IntStack() {
-    init(SIZE);
-  }
+IntStack::~IntStack()
+{
+	delete stack;
+}
 
-	IntStack::IntStack(int size) {
-		init(size);
-	}
+IntStack::IntStack(const IntStack& copied)
+{
+	stack = new int[copied.stackLength];
+	stackLength = copied.stackLength;
+	memcpy(stack, copied.stack, sizeof(int) * stackLength);
+	top = copied.top;
+}
 
-	void IntStack::init(int size)
+IntStack IntStack::operator=(const IntStack& copied)
+{
+	delete stack;
+	stack = new int[copied.stackLength];
+	stackLength = copied.stackLength;
+	memcpy(stack, copied.stack, sizeof(int) * stackLength);
+	top = copied.top;
+	return *this;
+}
+
+int IntStack::getTop() const
+{
+	return top;
+}
+
+int IntStack::setTop(int top) 
+{
+	int oldTop = this->top;
+	this->top = top;
+	return oldTop;
+}
+
+void IntStack::clear() 
+{
+	//for (int i = 0; i <= top; i++)
+	//stack[i] = 0;
+	top = -1;
+}
+
+bool IntStack::isEmpty() const
+{
+	return top < 0;
+}
+
+/**
+ * Pushes an element - top is incremented first than the
+ * element is assigned. This means top point to the last assigned
+ * element - which can be returned with peek().
+ */
+void IntStack::push(const int i) 
+{
+	// IO.dump("push:"+i);
+	if (++top >= stackLength) 
 	{
-		stack.resize(size);
-		clear();
+		expand();
 	}
-	
-	int IntStack::getTop() {
-    return top;
-  }
+	stack[top] = i;
+}
 
-   int IntStack::setTop( int top) {
-    return this->top = top;
-  }
+int IntStack::pop() 
+{
+	int r = stack[top--];
+	shrink();
+	return r;
+}
 
-   void IntStack::clear() {
-    top = -1;
-  }
+int IntStack::get(const int i) const
+{
+	return stack[i];
+}
 
-   bool IntStack::isEmpty() {
-    return top < 0;
-  }
+void IntStack::set(const int i, const int val)
+{
+	stack[i] = val;
+}
 
-  /**
-   * Pushes an element - top is incremented first than the
-   * element is assigned. This means top point to the last assigned
-   * element - which can be returned with peek().
-   */
-  void IntStack::push(int i) {
-    // IO.dump("push:"+i);
-    if (++top >= stack.size()) {
-      expand();
-    }
-    stack[top] = i;
-  }
+int IntStack::size() const
+{
+	return top + 1;
+}
 
-  int IntStack::pop() {
-    int r = stack[top--];
-    shrink();
-    return r;
-  }
+/*
+int IntStack::stackSize() const
+{
+	return stackLength;
+}
+*/
 
-  int IntStack::get(int i) {
-    return stack[i];
-  }
+/**
+ * dynamic array operation: doubles when full
+ */
+void IntStack::expand() 
+{
+	const int l = stackLength;
+	int* newstack = new int[l << 1];
 
-  void IntStack::set(int i, int val) {
-    stack[i] = val;
-  }
+	memcpy(newstack, stack, sizeof(int)*l);
+	delete stack;
+	stack = newstack;
+	stackLength = l << 1;
+}
 
-  int IntStack::size() {
-    return top + 1;
-  }
+/**
+ * dynamic array operation: shrinks to 1/2 if more than than 3/4 empty
+ */
+void IntStack::shrink() 
+{
+	int l = stackLength;
+	if (l <= MINSIZE || top << 2 >= l)
+		return;
 
-
-  /**
-   * dynamic array operation: doubles when full
-   */
-    void IntStack::expand() {
-     int l = stack.size();
-	 vector<int> newstack(l << 1);
-	 for (int i = 0; i < l; i++)
-	 {
-		 newstack[i] = stack[i];
-	 }
-	 stack = newstack;
-  }
-
-  /**
-  * dynamic array operation: shrinks to 1/2 if more than than 3/4 empty
-  */
-   void IntStack::shrink() {
-    int l = stack.size();
-    if (l <= MINSIZE || top << 2 >= l)
-      return;
-    l = 1 + (top << 1); // still means shrink to at 1/2 or less of the heap
-    if (top < MINSIZE) {
-      l = MINSIZE;
-    }
-
-	vector<int> newstack(l);
-	for (int i = 0; i < top + 1; i++)
+	l = 1 + (top << 1); // still means shrink to at 1/2 or less of the heap
+	if (top < MINSIZE) 
 	{
-		newstack[i] = stack[i];
+		l = MINSIZE;
 	}
-    //System.arraycopy(stack, 0, newstack, 0, top + 1);
-    stack = newstack;
-  }
 
-  vector<int> IntStack::toArray() {
-	  vector <int> array(size());
-    if (size() > 0) {
-		for (int i = 0; i < size(); i++)
-			array[i] = stack[i];
-    }
-    return array;
-  }
-
-  void IntStack::reverse() {
-    int l = size();
-    int h = l >> 1;
-    for (int i = 0; i < h; i++) {
-      int temp = stack[i];
-      stack[i] = stack[l - i - 1];
-      stack[l - i - 1] = temp;
-    }
-  }
-
-   string IntStack::toString() {
-	  vector<int> v = toArray();
-	  string s = "{";
-	  for (int i = 0; i < v.size(); i++)
-	  {
-		  if (i > 0)
-		  {
-			  s += ", ";
-		  }
-		  char tmp[10];
-		  sprintf(tmp, "%d", v[i]);
-		  s += tmp;
-	  }
-	  s += "}";
-	  return s;
-    //return Arrays.toString(toArray());
-  }
-
+	int* newstack = new int[l];
+	memcpy(newstack, stack, sizeof(int)*l);
+	delete stack;
+	stack = newstack;
+	stackLength = l;
+}
